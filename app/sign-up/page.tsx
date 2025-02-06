43b5a9483bf7f9c8be24dec810ee0e31018ff17b
netizen-om <borisagarom@gmail.com>
2025-02-06 22:00:37 +0530

    clerk custom sign-up logic complete

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -12,6 +12,7 @@ import {
     CardHeader,
     CardTitle,
   } from "@/components/ui/card"
+import { json } from 'stream/consumers'
 
 
 function Signup() {
@@ -26,9 +27,57 @@ function Signup() {
 
     const router = useRouter()
 
-  return (
-    <div>Signup</div>
-  )
+    async function submit(e : React.FormEvent){
+      e.preventDefault();
+      
+      if(!isLoaded){
+        return;
+      }
+
+      try {
+        await signUp.create({
+          emailAddress,
+          password
+        })
+
+        await signUp.prepareEmailAddressVerification({
+          strategy : "email_code"
+        })
+
+        setPendingVerification(true)
+
+      } catch (error : any) {
+        console.log(JSON.stringify(error, null, 2));
+        setError(error.errors[0])
+      }
+
+    }
+
+    async function onPressVerify(e : React.FormEvent) {
+      e.preventDefault()
+      if(!isLoaded) {
+        return
+      }
+
+      try {
+        const completeSigUp = await signUp.attemptEmailAddressVerification({code})
+
+        if(completeSigUp.status !== "complete"){
+          console.log(JSON.stringify(completeSigUp, null, 2));
+          setError("Email verification fail")
+        }
+
+        if(completeSigUp.status === "complete") {
+          await setActive({session : completeSigUp.createdSessionId})
+          router.push("/dashboard")
+        }
+
+      } catch (error : any) {
+        console.log(JSON.stringify(error, null, 2));
+        setError(error.errors[0])
+      }
+
+    }
 }
 
 export default Signup
