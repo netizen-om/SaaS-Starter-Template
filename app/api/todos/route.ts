4921c663216332d7896f19edeb25ab67951bfe19
netizen-om <borisagarom@gmail.com>
2025-02-16 21:45:18 +0530

    get all todos and pagination complete

---
+++ app/api/todos/route.ts
@@ -0,0 +1,79 @@
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+const ITEMS_PER_PAGE = 10;
+
+export async function GET(req : NextRequest) {
+    const { userId } = await auth()
+
+    if(!userId) {
+        return NextResponse.json({error : "Unauthorized"}, { status : 401 })
+    }
+
+    const { searchParams } = new URL(req.url)
+    const page = parseInt(searchParams.get("page") || "1")
+    const search = searchParams.get("search") || ""
+
+    try {
+        const todos = await prisma.todo.findMany({
+            where : {
+                userId,
+                title : {
+                    contains : search,
+                    mode : "insensitive"
+                } 
+            },
+            orderBy : { createdAt : "desc"},
+            take : ITEMS_PER_PAGE,
+            skip : (page - 1)*ITEMS_PER_PAGE
+        })
+
+        const totalItems = await prisma.todo.count({
+            where : { 
+                userId,
+                title : {
+                    contains : "search",
+                    mode : "insensitive"
+                }
+            }
+        })
+
+        const totalPages = Math.ceil(totalItems/ITEMS_PER_PAGE)
+
+        return NextResponse.json({
+            todos,
+            currentPage : page,
+            totalItems,
+            totalPages
+        })
+
+    } catch (error) {
+        console.error("Error geting Todos : " + error);
+        return NextResponse.json({ error : "Internal server error"}, { status : 500 })
+    }
+}
+
+export async function POST(req : NextRequest) {
+    const { userId } = await auth()
+
+    if(!userId) {
+        return NextResponse.json({error : "Unauthorized"}, { status : 401 })
+    }
+    
+    const user = await prisma.user.findUnique({
+        where : { id : userId },
+        include : { todos : true }
+    })
+    
+    if(!user) {
+        return NextResponse.json({error : "User not found"}, { status : 404 })
+    }
+    
+    if(!user.isSubscribed && user.todos.length >= 3) {
+        return NextResponse.json({error : "Free users can onlu create 3 todos please subscribe to use all service"}, { status : 403 })
+    }
+
+    await req.json()
+
+}
